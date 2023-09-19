aa05861b54ae9c26aa69c57162aea8d61c7694a5
Imre Szepesi <imijuhu@gmail.com>
2023-09-19 12:57:03 +0200

    Első sikeres AJAX hivás

---
+++ zokniParosito/wwwroot/js/Jatek/JatekAdatok.js
@@ -0,0 +1,14 @@
+
+function GetZoknik() {
+    $.ajax({
+        url: '/Jatek/GetZoknik',
+        type: "GET",
+        success: function (data) {
+            let x = JSON.stringify(data);
+            console.log(x);
+        },
+        error: function (error) {
+            console.log(`Error ${error}`);
+        }
+    });
+}
